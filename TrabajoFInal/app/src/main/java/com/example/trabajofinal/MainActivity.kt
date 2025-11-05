8dd88d9fe809e90bdfb014d0837a8a56bef3e0bc
sadesuu <hsolistorrijos@gmail.com>
2025-11-05 11:35:25 +0100

    Add character management features with CRUD operations and UI components

---
+++ TrabajoFInal/app/src/main/java/com/example/trabajofinal/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.trabajofinal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.trabajofinal.ui.fragments.CharacterListFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Load the character list fragment
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, CharacterListFragment())
+                .commit()
+        }
+    }
+}
